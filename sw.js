cca42f84757f18b7f8e9e36c76f33e1586b531b9
jeffrey9jeff <152591680+jeffrey9jeff@users.noreply.github.com>
2025-08-09 21:24:17 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'georgia-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/georgia-avatar.jpg' // optional; ok if missing
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS.filter(Boolean))));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp=>{
+      // cache GET requests on the fly
+      const copy = resp.clone();
+      if (e.request.method === 'GET' && resp.status===200) {
+        caches.open(CACHE).then(c=>c.put(e.request, copy));
+      }
+      return resp;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
